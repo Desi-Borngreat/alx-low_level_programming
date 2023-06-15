b6c023afedf870f95176bf289629dd470dc4a78a
Desi-Borngreat <allweda95@gmail.com>
2023-06-15 10:56:43 -0700

    C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program,  Print a string
+ * Description: using  printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
