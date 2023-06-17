c18d1c012799cc3d6dccc803de3afd4c0e020fa6
Desi-Borngreat <allweda95@gmail.com>
2023-06-16 22:01:55 -0700

    task 2 to 9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+
+	putchar('\n');
+	return (0);
