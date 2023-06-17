7016b2f25b1f74eb85fe4af4d5179f45a92b9d87
Desi-Borngreat <allweda95@gmail.com>
2023-06-16 22:11:34 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
  */
-
 int main(void)
 {
 	char c;
+	char d = '0';
 
-	int d;
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
 
-	c = 'a';
-	d = 0;
-	while
-		(d < 10) {
-			putchar(d + '0');
-			d++;
-		}
-	while
-		(c <= 'f') {
-			putchar(c);
-			c++;
-		}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
 	putchar('\n');
 	return (0);
+}
